19de25de69befe39fd9dabdcc582a824349a7ed7
AnonyHer <riers23@gmail.com>
2025-06-27 09:47:49 +0700

    Membuat fake image dan user

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Photo;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Photo>
+ */
+class PhotoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+         $width = fake()->numberBetween(300, 800);
+        $height = fake()->numberBetween(300, 800);
+        $category = fake()->unique()->numberBetween(1, 1000);
+
+        $imageUrl = "https://picsum.photos/{$width}/{$height}?random={$category}";
+        return [
+            'username' => \App\Models\User::factory(),
+            'url' => $imageUrl,
+            'caption' => fake()->sentence(),
+            'is_public' => true,
+            'is_path' => true,
+        ];
+    }
+
+    public function is_path(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_path' => false,
+        ]);
+    }
+}
