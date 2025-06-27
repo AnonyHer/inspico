7e813d2bb5cd002848279ff54af64fb520ef81e3
AnonyHer <riers23@gmail.com>
2025-06-27 09:48:47 +0700

    Membuat data faker image dan user

---
+++ database/migrations/2025_06_25_122153_create_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('username')->constrained(
+                'users',
+                'id'
+            );
+            $table->boolean('is_public')->default(true);
+            $table->boolean('is_path')->default(true);
+            $table->string('caption')->nullable();
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
